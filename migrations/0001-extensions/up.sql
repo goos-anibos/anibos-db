1e19f458f12b10f512a8012cc16d2d91c2af806e
yingniu <yinghuan.niu@shaw.ca>
2026-07-29 08:06:24 -0600

    Added more postgis extensions

--- migrations/0001-extensions/up.sql
+++ migrations/0001-extensions/up.sql
@@ -1,4 +1,6 @@
 -- SQL ALTER statements for database migration
 CREATE EXTENSION if not exists postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
+CREATE EXTENSION IF NOT EXISTS postgis_raster;
 CREATE EXTENSION if not exists dblink;
 CREATE EXTENSION IF NOT EXISTS timescaledb;
